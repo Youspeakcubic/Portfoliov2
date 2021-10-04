92ce89e7c8417cdb0d17a1733dd5abbe36adf46b
Anthony <delantho327@gmail.com>
2021-10-04 04:22:25 -0500

    changed landing jumbotron to use grid display, made lander more responsive

--- assets/javascript/typewriter.js
+++ assets/javascript/typewriter.js
@@ -1,16 +1,15 @@
-console.log("hey");
-var typewrite = document.getElementById('typeWriterContainer');
 
+var typewrite = document.getElementById('typeWriterContainer');
 var typewriter = new Typewriter(typewrite, {
     loop: true
 });
 
-typewriter.typeString('<Strong>Hello my name is Anthony</strong>')
+typewriter.typeString('<Strong>	&#5760 Hello my name is Anthony</strong>')
     .pauseFor(2500)
-    .deleteAll()
-    .typeString('<Strong>I am a passionate Front end devoper</strong>')
+    .deleteChars(24)
+    .typeString('<Strong> &#5760 I am a passionate Front end developer</strong>')
     .pauseFor(2500)
-    .deleteAll()
+    .deleteChars(37)
     .typeString('<Strong>Specializing in Javascript and bootstrap</strong>')
     .pauseFor(2500)
     .start();
